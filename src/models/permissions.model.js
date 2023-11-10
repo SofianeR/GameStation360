1e8251b2cbdb0372b81bb659acdc710669f6781b
Noah <89213023+chifouu65@users.noreply.github.com>
2023-11-10 16:15:59 +0100

    Create permissions.model.js

---
+++ src/models/permissions.model.js
@@ -0,0 +1,10 @@
+const Permissions = [
+  "create-game", 
+  "join-game",
+  "kick-game",
+  "end-game",
+  "create-game",
+  "remove-chat-message",
+  "send-chat-message",
+  "read-chat-message"
+]
