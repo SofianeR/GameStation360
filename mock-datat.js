fee53ba31e3724a912402a442220bf13a2c57c1f
Noah <89213023+chifouu65@users.noreply.github.com>
2023-11-10 15:44:10 +0100

    Create mock-datat.js

---
+++ mock-datat.js
@@ -0,0 +1,17 @@
+[{
+  username: "theo",
+  password: "gyuhjikl",
+  roles: [],
+  token: "string"
+},{
+  username: "math",
+  password: "gyuhjikl",
+  roles: [],
+  token: "string"
+},
+{
+  username: "leo",
+  password: "gyuhjikl",
+  roles: [],
+  token: "string"
+}]
